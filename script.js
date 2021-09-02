2bd94830cc2e336babf2d9e99196b1aea3ab0835
Rasel <hauladerrasel2@gmail.com>
2021-09-02 13:33:20 +0600

    added more

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ const loadBooks = () => {
         document.getElementById('total-found').innerText = 'Please provide a book name'
         return
     }
-   
+
     document.getElementById('book-container').textContent = '';
     document.getElementById('total-found').textContent = '';
     console.log(inputValue)
     document.getElementById('spinner').classList.remove('d-none')
-    fetch(`http://openlibrary.org/search.json?q=${inputValue}`)
+    fetch(`https://openlibrary.org/search.json?q=${inputValue}`)
         .then(res => res.json())
         .then(data => displayBooks(data))
 }
@@ -17,33 +17,32 @@ const displayBooks = books => {
     document.getElementById('input-value').value = '';
     console.log(books)
     document.getElementById('spinner').classList.add('d-none')
-   if (books.numFound) {
-    document.getElementById('total-found').innerText =`Total result found ${books.numFound}`
-   }else{
-    document.getElementById('total-found').innerText =`No Result Found` 
-   }
+    if (books.numFound) {
+        document.getElementById('total-found').innerText = `Total result found ${books.numFound}`
+    } else {
+        document.getElementById('total-found').innerText = `No Result Found`
+    }
     const container = document.getElementById('book-container');
     books.docs.forEach(book => {
         let author = 'unknown'
         let img = ''
         if (book.author_name) {
-            author =  book.author_name[0]
+            author = book.author_name[0]
         }
         if (book.cover_i) {
-             img = `<img height="300px" src="https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg" class="card-img-top" alt="..."> `
-        }else{
-             img = `<img height="300px" src="image-not-found.jpg" class="card-img-top" alt="..."> `
+            img = `<img height="250px" width="160px" src="https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg" class="mx-auto mt-4" alt="..."> `
+        } else {
+            img = `<img height="250px" width="160px" src="image-not-found.jpg" class="mx-auto mt-4" alt="..."> `
         }
-        
-
         const div = document.createElement('div');
         div.className = 'col'
         div.innerHTML = ` 
         <div class="card h-100">
         ${img}
         <div class="card-body">
-            <p class="card-title fs-3">${book.title}</p>
-            <p class="fs-5">by ${author}</p>
+            <p class="card-title fs-3 fw-bolder">${book.title}</p>
+            <p class="fs-5">by <span class="text-primary"> ${author} </span></p>
+            <p>Publisher : ${book.publisher ? book.publisher[0] : 'unknown'} </p>
             <p>First published in ${book.first_publish_year}</p>
            </div>
         
@@ -51,5 +50,5 @@ const displayBooks = books => {
         `
         container.appendChild(div)
     });
-    
+
 }
